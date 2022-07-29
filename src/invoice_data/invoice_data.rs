57f12684e0e5573c46229d36ca6e5688c03d3f3d
Karol Moroz <karol@moroz.dev>
2022-07-30 01:00:43 +0200

    Add tests for parsing Party and InvoiceData

--- src/invoice_data/invoice_data.rs
+++ src/invoice_data/invoice_data.rs
@@ -8,3 +8,37 @@ pub struct InvoiceData {
     seller: Party,
     buyer: Party,
 }
+
+#[test]
+fn test_deserialize_invoice_data() {
+    let input = r#"
+    {
+        "buyer": {
+            "name": "Test Buyer",
+            "tax_id": "123456",
+            "address_first_line": "123, Some Street",
+            "address_city": "SimCity",
+            "address_zip_code": "DE12345"
+        },
+        "seller": {
+            "name": "Test Seller",
+            "tax_id": "123456",
+            "address_first_line": "123, Some Street",
+            "address_city": "SimCity",
+            "address_zip_code": "DE12345"
+        },
+        "items": [
+            {
+                "title": "Software Development",
+                "amount": 1,
+                "unit_price": "21.37",
+                "vat_rate": "FIVE"
+            }
+        ]
+    }
+    "#;
+
+    let actual: InvoiceData = serde_json::from_str(&input).unwrap();
+
+    assert_eq!(actual.items.len(), 1);
+}
--- src/invoice_data/party.rs
+++ src/invoice_data/party.rs
@@ -8,3 +8,24 @@ pub struct Party {
     address_city: String,
     address_zip_code: String,
 }
+
+#[test]
+fn test_deserialize_party() {
+    let input = r#"
+        {
+            "name": "Test Buyer",
+            "tax_id": "123456",
+            "address_first_line": "123, Some Street",
+            "address_city": "SimCity",
+            "address_zip_code": "DE12345"
+        }
+    "#;
+
+    let actual: Party = serde_json::from_str(&input).unwrap();
+
+    assert_eq!(actual.name, "Test Buyer");
+    assert_eq!(actual.tax_id, Some("123456".to_string()));
+    assert_eq!(actual.address_first_line, "123, Some Street");
+    assert_eq!(actual.address_city, "SimCity");
+    assert_eq!(actual.address_zip_code, "DE12345");
+}
