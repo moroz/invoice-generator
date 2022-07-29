e9812b28e2a52b1c109e54714896b491778e3d37
Karol Moroz <karol@moroz.dev>
2022-07-29 22:18:23 +0200

    Test deserializing invoice items

--- src/invoice_data/invoice_item.rs
+++ src/invoice_data/invoice_item.rs
@@ -1,5 +1,6 @@
 use crate::invoice_data::VatRate;
 use rust_decimal::prelude::*;
+use rust_decimal_macros::*;
 use serde::{Deserialize, Serialize};
 
 #[derive(Debug, Serialize, Deserialize)]
@@ -8,4 +9,22 @@ pub struct InvoiceItem {
     amount: u32,
     unit_price: Decimal,
     vat_rate: VatRate,
+}
+
+#[test]
+fn test_invoice_item_deserialization() {
+    let input = r#"
+        {
+            "title": "Software Development",
+            "amount": 1,
+            "unit_price": "21.37",
+            "vat_rate": "FIVE"
+        }
+    "#;
+
+    let actual: InvoiceItem = serde_json::from_str(&input).unwrap();
+    assert_eq!("Software Development", actual.title);
+    assert_eq!(1, actual.amount);
+    assert_eq!(dec!(21.37), actual.unit_price);
+    assert_eq!(VatRate::Five, actual.vat_rate);
 }
