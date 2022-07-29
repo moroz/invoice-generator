3cf51ec389693c809f52fb0ee48382a999845a51
Karol Moroz <karol@moroz.dev>
2022-07-29 21:02:49 +0200

    Create output directory

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
-fn main() {
+use std::env;
+use std::fs;
+use std::io;
+use std::path::PathBuf;
+
+fn resolve_out_dir() -> io::Result<PathBuf> {
+    let cwd = env::current_dir()?;
+    let default = PathBuf::from("/");
+    let parent = cwd.parent().unwrap_or(&default);
+    Ok(parent.join("out"))
+}
+
+fn main() -> io::Result<()> {
+    let path = resolve_out_dir()?;
+    println!("{:?}", path);
+    fs::create_dir_all(path)?;
     println!("Hello, world!");
+
+    Ok(())
 }
