942a8924ded4780508f8f11edc7e08eec3d05351
Karol Moroz <karol@moroz.dev>
2022-07-29 21:47:14 +0200

    Implement custom JSON serializer for vat rate

--- src/invoice_data/vat_rate.rs
+++ src/invoice_data/vat_rate.rs
@@ -1,6 +1,6 @@
-use serde::{Deserialize, Serialize};
+use serde::{Deserialize, Deserializer, Serialize, Serializer};
 
-#[derive(Debug, Serialize, Deserialize)]
+#[derive(Debug, Deserialize)]
 pub enum VatRate {
     NA,
     Zero,
@@ -10,12 +10,39 @@ pub enum VatRate {
     TwentyThree,
 }
 
+impl Serialize for VatRate {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: Serializer,
+    {
+        let value = match *self {
+            VatRate::NA => "N_A",
+            VatRate::Zero => "ZERO",
+            VatRate::Five => "FIVE",
+            VatRate::Seven => "SEVEN",
+            VatRate::Eight => "EIGHT",
+            VatRate::TwentyThree => "TWENTY_THREE",
+        };
+
+        serializer.serialize_str(value)
+    }
+}
+
+// impl<'de> Deserialize<'de> for VatRate {
+//     fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+//     where
+//         D: Deserializer<'de>,
+//     {
+//         let value = deserializer.deserialize_str({d});
+//     }
+// }
+
 #[test]
 fn test_vat_rate_serialization() {
     let actual = serde_json::to_string(&VatRate::NA).unwrap();
-    assert_eq!("\"NA\"", actual);
+    assert_eq!("\"N_A\"", actual);
     let actual = serde_json::to_string(&VatRate::Zero).unwrap();
-    assert_eq!("\"Zero\"", actual);
+    assert_eq!("\"ZERO\"", actual);
     let actual = serde_json::to_string(&VatRate::TwentyThree).unwrap();
-    assert_eq!("\"TwentyThree\"", actual);
+    assert_eq!("\"TWENTY_THREE\"", actual);
 }
